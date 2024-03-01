c6dd17c19b4f7a16da24d77344f1321dd4499269
adeelrao007 <adeelrao007@gmail.com>
2024-03-01 19:34:03 +0500

    sample code files - middleware

---
+++ Middleware/AllowWebUsersOnly.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AllowWebUsersOnly
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user()->role_id == 2) {
+            Auth::logout();
+            Session::flush();
+            abort(403, trans('users.mobile_users_are_not_allowed_to_login_into_web_portal'));
+        }
+
+        return $next($request);
+    }
+}
